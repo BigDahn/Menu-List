4f839c78bb35e050360cbf1686f8041d7114ca22
BigDahn <harkinborholarholuwah@gmail.com>
2024-08-07 10:06:39 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import App from './Menu/App'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
-import { Provider } from 'react-redux'
-import {  store } from './store'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Provider store={store}>
+    
       
         <App />
         <ToastContainer position="top-center" />
       
-    </Provider>
+   
   </>
 )
